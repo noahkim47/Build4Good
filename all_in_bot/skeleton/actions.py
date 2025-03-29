a0296e600ed424281f1720999c4144e7f34e7e9c
noahkim47 <noah.kim@tamu.edu>
2025-03-29 09:26:31 -0500

    asdf

--- all_in_bot/skeleton/actions.py
+++
@@ -1,9 +0,0 @@
-'''
-The actions that the player is allowed to take.
-'''
-from collections import namedtuple
-
-FoldAction = namedtuple('FoldAction', [])
-CallAction = namedtuple('CallAction', [])
-CheckAction = namedtuple('CheckAction', [])
-RaiseAction = namedtuple('RaiseAction', ['amount'])
